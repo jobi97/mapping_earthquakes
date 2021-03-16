6fd1579b833e752d225fdcc9e95f9042fd7fdb66
jobi97 <74573826+jobi97@users.noreply.github.com>
2021-03-16 19:57:54 -0400

    Add files via upload

---
+++ mapping_multiple_points/static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1Ijoiam9iaTk3IiwiYSI6ImNrbHU2cnd1aDF1dXMyb2x3eDZ5aTgwbjMifQ.-KB8wTg6RNng6M5_ESKR6Q";
---
+++ mapping_multiple_points/static/js/logic.js
@@ -0,0 +1,26 @@
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+  
+//get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+   });
+
+
+  // We create the tile layer that will be the background of our map.
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+  });
+  
+  // Then we add our 'graymap' tile layer to the map.
+  streets.addTo(map);
